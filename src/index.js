b516a60006b597bfc48e04441a1dca671978f163
Sandesh Jain <81623367+SandeshJ-07@users.noreply.github.com>
2021-05-23 21:27:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import {GithubProvider} from './context/context';
 import {Auth0Provider} from '@auth0/auth0-react';
 
